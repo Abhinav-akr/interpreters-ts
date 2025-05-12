e2084728bd2e0e47c66e5357b16a8c3732be455c
Abhinav Kumar <abhinav.kumar@prismforce.ai>
2025-05-13 04:29:46 +0530

    Map parentheses tokens to descriptive strings in tokenizer.
    Added handling for "(" and ")" tokens in the tokenizer function. This ensures consistent mapping of tokens to descriptive strings alongside existing brace handling. It also improves token differentiation, aiding downstream processes.

--- app/main.ts
+++ app/main.ts
@@ -31,7 +31,9 @@ if (fileContent.length !== 0) {
   //scanAndLogBraces(tokens);
   const tokenizedArr: string[] = tokens.map((token) => {
     if (token === "{") return "LEFT_BRACE { null";
+    else if (token === "(") return "LEFT_PAREN ( null";
     else if (token === "}") return "RIGHT_BRACE } null";
+    else if (token === ")") return "RIGHT_PAREN ) null";
     else return "UNKNOWN"
   });
   tokenizedArr.push("EOF  null");
